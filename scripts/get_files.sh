133aa13262f40798e070660d71a231753eeb2a73
root <root@stk>
2019-05-18 19:04:33 +0000

    Helper scriptis to take care of the install
    wget script to make fetching the required files a bit easier.
    
    Bits and pieces need to be moved to the correct directories and
    have the correct permissions. This script takes care of that.

---
+++ scripts/get_files.sh
@@ -0,0 +1,6 @@
+wget https://github.com/smiths73v3/overlayFS/raw/master/init.d/saveoverlays-stretch
+wget https://github.com/smiths73v3/overlayFS/raw/master/utils/rootro
+wget https://github.com/smiths73v3/overlayFS/raw/master/init.d/syncoverlayfs.service
+wget https://github.com/smiths73v3/overlayFS/raw/master/scripts/install_parts.sh
+wget https://github.com/smiths73v3/overlayFS/raw/master/scripts/movefs.sh
+
---
+++ scripts/install_parts.sh
@@ -0,0 +1,8 @@
+mv saveoverlays-stretch saveoverlays
+chmod a+rx saveoverlays mount_overlay rootro movefs.sh
+cp mount_overlay /usr/local/bin/
+cp saveoverlays /etc/init.d/ 
+cp rootro /usr/local/bin/ 
+ln -s rootro /usr/local/bin/rootrw 
+cp syncoverlayfs.service /lib/systemd/system/
+
