b63802fbc9863f46a94095924c9c13eb1a15b076
root <root@stk>
2019-05-18 21:59:04 +0000

    Add +x execute permissions for "install_parts.sh"
    Remove an additional chmod for install_parts.sh before it can be
    run.

--- scripts/get_files.sh
+++ scripts/get_files.sh
@@ -3,4 +3,5 @@ wget https://github.com/smiths73v3/overlayFS/raw/master/utils/rootro
 wget https://github.com/smiths73v3/overlayFS/raw/master/init.d/syncoverlayfs.service
 wget https://github.com/smiths73v3/overlayFS/raw/master/scripts/install_parts.sh
 wget https://github.com/smiths73v3/overlayFS/raw/master/scripts/movefs.sh
+chmod a+rx install_parts.sh
 
