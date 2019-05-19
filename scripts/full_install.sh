1a4fe1aec28fdd5d1edaeb9eab25086d5d4343e0
root <root@stk>
2019-05-19 00:37:26 +0000

    Full install helper script
    Add helper script to do full_install for those wanting to just
    hit a "go" button...

---
+++ scripts/full_install.sh
@@ -0,0 +1,34 @@
+#
+#Disable Swap
+dphys-swapfile swapoff
+dphys-swapfile uninstall
+systemctl disable dphys-swapfile
+
+#Fetch files and install apt packages
+wget https://github.com/smiths73v3/overlayFS/raw/master/scripts/get_files.sh
+chmod a+rx get_files.sh
+./get_files.sh
+apt-get install fuse lsof
+
+#start the installs
+./install_parts.sh
+systemctl daemon-reload
+systemctl enable syncoverlayfs.service
+
+#fixup /boot/cmdline.txt
+sed -i.bak -e "s/$/ noswap fastboot ro/" /boot/cmdline.txt
+
+#update fstab
+./fixup_fstab.sh
+
+#move fs
+echo "Long Operation in progress, moving filesystems"
+./movefs.sh
+
+#mounts
+mount /home
+mount /root
+mount /var
+mount -v | grep "overlay"
+
+
