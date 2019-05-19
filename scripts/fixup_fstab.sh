714bd4d405377a75213b5db7ae4ffc6eefb806f2
root <root@stk>
2019-05-19 00:17:44 +0000

    Add script to deal with /etc/fstab changes
    fstab edits are a little tricky, but we can handle all of it with
    a script.

---
+++ scripts/fixup_fstab.sh
@@ -0,0 +1,6 @@
+sed -i.orig -e "s/\(PARTUUID.*\)\(defaults\)/\1ro/" /etc/fstab
+echo "mount_overlay	/home	fuse	nofail,defaults	0	0" >>/etc/fstab
+echo "mount_overlay	/root	fuse	nofail,defaults	0	0" >>/etc/fstab
+echo "mount_overlay	/var	fuse	nofail,defaults	0	0" >>/etc/fstab
+echo "none		/tmp	tmpfs	defaults	0	0" >>/etc/fstab
+
