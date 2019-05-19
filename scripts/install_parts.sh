227a85cb64c26447af3f3312617cad45a3218ac5
root <root@stk>
2019-05-19 01:46:39 +0000

    Fix permissions for fix_fstab.sh

--- scripts/install_parts.sh
+++ scripts/install_parts.sh
@@ -1,5 +1,5 @@
 mv saveoverlays-stretch saveoverlays
-chmod a+rx saveoverlays mount_overlay rootro movefs.sh
+chmod a+rx saveoverlays mount_overlay rootro movefs.sh fixup_fstab.sh
 cp mount_overlay /usr/local/bin/
 cp saveoverlays /etc/init.d/ 
 cp rootro /usr/local/bin/ 
