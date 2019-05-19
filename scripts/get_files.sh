797d2538c6c34a5d4a32c3d4c30d511e862753c9
root <root@stk>
2019-05-19 00:21:04 +0000

    Add fixup_fstab.sh to the wget list

--- scripts/get_files.sh
+++ scripts/get_files.sh
@@ -4,5 +4,6 @@ wget https://github.com/smiths73v3/overlayFS/raw/master/init.d/saveoverlays-stre
 wget https://github.com/smiths73v3/overlayFS/raw/master/init.d/syncoverlayfs.service
 wget https://github.com/smiths73v3/overlayFS/raw/master/scripts/install_parts.sh
 wget https://github.com/smiths73v3/overlayFS/raw/master/scripts/movefs.sh
+wget https://github.com/smiths73v3/overlayFS/raw/master/scripts/fixup_fstab.sh
 chmod a+rx install_parts.sh
 
