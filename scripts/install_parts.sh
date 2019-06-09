143b3b01e19c264e9eae9d9d8d1f642e44889422
SteveS <smiths.73v3@gmail.com>
2019-06-08 23:53:25 -0400

    move some /etc files to /var/local
    Create softlinks in a readable area /var/local so that files that
    are either commonly updated, or are changed by Zynthian are always rw.

--- scripts/install_parts.sh
+++ scripts/install_parts.sh
@@ -6,3 +6,16 @@ cp rootro /usr/local/bin/
 ln -s rootro /usr/local/bin/rootrw 
 cp syncoverlayfs.service /lib/systemd/system/
 
+mv /etc/wpa_supplicant/wpa_supplicant.conf /var/local/
+ln -s /var/local/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+mv /etc/resolv.conf /var/local/
+ln -s /var/local/resolv.conf /etc/resolv.conf
+touch /var/local/resolv.conf.bak
+rm /etc/resolv.conf.bak
+ln -s /var/local/resolv.conf.bak /etc/resolv.conf.bak
+mv /etc/fake-hwclock.data /var/local/
+ln -s /var/local/fake-hwclock.data /etc/fake-hwclock.data
+touch /var/local/mtab
+rm /etc/mtab
+ln -s /var/local/mtab /etc/mtab
+
